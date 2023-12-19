b58ff277702f478ad11ca7f0fda58c8d0d09ccf4
abiboni <abigail.o.bonilla@gmail.com>
2023-12-19 11:31:58 -0500

    final final

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import satData from "./components/satData";
 
 // const satData = satData
+// imported useState and satData to access data
 
 function App() {
   const [sat, setSat] = useState(satData);
